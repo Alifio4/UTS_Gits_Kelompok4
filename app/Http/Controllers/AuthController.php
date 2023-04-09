ce83db6993887a7f03b6f78e5276fbc96cfc6ea0
Patria Akbar <patriaakbar1306@gmail.com>
2023-04-09 14:30:03 +0700

    membuat product edit dan add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('home');
+            return redirect()->intended('/');
         }
         return back()->withErrors([
             'email' => 'Email not found',
