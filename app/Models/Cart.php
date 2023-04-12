8d6cdbaa91326d4d538ea8199179305c281204d7
Alifio4 <fionugroho4@students.unnes.ac.id>
2023-04-12 21:04:19 +0700

    Uppdate_relasi_cart
    Menu per user

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -18,5 +19,8 @@ class Cart extends Model
 
     public function cart(){
         return $this->belongsTo(Product::class, 'product_id'); //menunggu tabel produk
+    }
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id'); //menunggu tabel produk
     }
 }
